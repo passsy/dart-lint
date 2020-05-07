f874f2f3f3c34fba9ee9d7edc7b0d7cb80d89b7a
Pascal Welsch <pascal@welsch.dev>
2020-05-07 11:05:28 +0200

    Document library
    Doesn't add a penalty on pub.dev

--- lib/lint.dart
+++ lib/lint.dart
@@ -1,2 +1,14 @@
-// This file prevents dartdoc from failing because this package doesn't
-// contain a library
+/// The lint package doesn't ship any dart source code.
+///
+/// To enable `lint`,
+/// 1. Add it to your dev_dependencies
+/// ```yaml
+/// dev_dependencies:
+///   lint: ^1.0.0
+/// ```
+///
+/// 2. Include the rules into your `analysis_options.yaml`
+/// ```yaml
+/// include: package:lint/analysis_options.yaml
+/// ```
+library lint;
